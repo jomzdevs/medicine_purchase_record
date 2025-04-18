6a06e09a7623f4f6d7fcb5b59f4ba9b0de4d371b
jomari divinagracia <jomaridivinagracia.bscs.pass@gmail.com>
2025-04-18 23:14:50 +0800

    added canPop on home.dart

--- lib/screens/homescreen/home.dart
+++ lib/screens/homescreen/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../lists/medicine_list.dart';
 import '../lists/purchased_list.dart';
 
@@ -23,24 +24,70 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Future<bool> _exitDialog() async {
+    final bool? shouldExit = await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Exit App'),
+        content: const Text('Are you sure? You want to exit MediTrack?'),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text(
+              'Cancel',
+              style: TextStyle(color: Colors.teal),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
+              child: const Text('Exit'))
+        ],
+      ),
+    );
+    if (shouldExit == true) {
+      SystemNavigator.pop();
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_long),
-            label: 'Purchases',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.medication),
-            label: 'Medicines',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.teal,
-        onTap: _onItemTapped,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, Object? result) async {
+        if (didPop) {
+          return;
+        }
+        final bool shouldPop = await _exitDialog();
+        if (shouldPop) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        body: _widgetOptions.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long),
+              label: 'Purchases',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.medication),
+              label: 'Medicines',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.teal,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
